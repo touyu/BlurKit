446ffd4ec096643353db74ee990660d44e364088
touyu <dev.akkey0222@gmail.com>
2018-10-05 22:55:27 +0900

    Add BlurKit.framework to Demo

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import BlurKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        
     }
 
 
