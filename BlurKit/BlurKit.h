edf4daf3c6959aa4ef4be43086fbda158497f04e
touyu <dev.akkey0222@gmail.com>
2018-10-05 21:49:36 +0900

    Initial Commit

---
+++ BlurKit/BlurKit.h
@@ -0,0 +1,19 @@
+//
+//  BlurKit.h
+//  BlurKit
+//
+//  Created by Yuto Akiba on 2018/10/05.
+//  Copyright © 2018 Yuto Akiba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BlurKit.
+FOUNDATION_EXPORT double BlurKitVersionNumber;
+
+//! Project version string for BlurKit.
+FOUNDATION_EXPORT const unsigned char BlurKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BlurKit/PublicHeader.h>
+
+
